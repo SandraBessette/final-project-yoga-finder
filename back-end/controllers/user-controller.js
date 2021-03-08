65a8233999b997093b567a6b9a30ecec8bd20983
Sandra <sandrabess@hotmail.com>
2021-03-08 14:18:28 -0500

    server structure

---
+++ back-end/controllers/user-controller.js
@@ -0,0 +1,10 @@
+
+
+export const signin = async (req, res) => {
+
+};
+
+
+export const signout = async (req, res) => {
+
+};
--- back-end/index.js
+++ back-end/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+import userRoutes from './routes/user-routes';
 
-
-const app = express();
 const PORT = 8000;
 
-app.use(function (req, res, next) {
+const app = express();
+app.use((cors()));
+
+
+/*app.use(function (req, res, next) {
     res.header(
       "Access-Control-Allow-Methods",
       "OPTIONS, HEAD, GET, PUT, POST, DELETE"
@@ -14,12 +18,14 @@ app.use(function (req, res, next) {
       "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
-  })
+  })*/
  
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use("/", express.static(__dirname + "/"))
+
+app.use("/user", userRoutes);
 
 app.get('/',(req, res)=>{
     res.status(200).json({ status: 200, message: "success"});
