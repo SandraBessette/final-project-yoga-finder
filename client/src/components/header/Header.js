2493991aa97d9adaf5d7ac99386534735238a73c
Sandra <sandrabess@hotmail.com>
2021-04-08 14:36:51 -0400

    put back socket endpoint

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -15,9 +15,9 @@ import { onSmallTabletMediaQuery, onSmallPhoneMediaQuery, onPhoneMediaQuery } fr
 import { receiveCountInfo, resetChat, updateMessage, increaseCountInfo } from '../../store/reducers/chat/actions';
 
 //const ENDPOINT = 'http://localhost:3000';
-//let socket = io(ENDPOINT);
-//let socket = io(process.env.REACT_APP_SOCKET_ENDPOINT);
-let socket = io('/');
+let socket = io(ENDPOINT);
+let socket = io(process.env.REACT_APP_SOCKET_ENDPOINT);
+//let socket = io('/');
 
 const Header = ()=>{
     const {authData} = useSelector((state)=>state.auth);  
